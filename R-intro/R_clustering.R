87af08741997eeece655c1e8d804595846b71097
mstambou <mstambou@indiana.edu>
2019-01-27 18:32:46 -0500

    created RNA-seq folder

--- R-intro/R_clustering.R
+++ R-intro/R_clustering.R
@@ -33,3 +33,30 @@ data <- data0[ -c(10) ]
 d <- dist(data, method = "euclidean") # distance matrix
 fit <- hclust(d, method="ward.D") 
 plot(fit, main="hierarchical clustering for cancer dataset") # display dendogram
+
+
+#using the mtcars data set
+#clean/normalize the data
+data(mtcars)
+mydata = na.omit(mtcars) #deletion of missing
+mydata = scale(mydata) #standarize variables
+
+# Determine number of clusters
+wss <- (nrow(mydata)-1)*sum(apply(mydata,2,var))
+for (i in 2:15) wss[i] <- sum(kmeans(mydata, 
+                                     centers=i)$withinss)
+plot(1:15, wss, type="b", xlab="Number of Clusters",
+     ylab="Within groups sum of squares")
+# check out the plot
+
+
+# K-Means Cluster Analysis
+fit <- kmeans(mydata, 5) # 5 cluster solution
+# get cluster means 
+aggregate(mydata,by=list(fit$cluster),FUN=mean)
+# append cluster assignment
+mydata <- data.frame(mydata, fit$cluster)
+
+#visualize the clustering results
+library(cluster) 
+clusplot(mydata, fit$cluster, color=TRUE, shade=TRUE, labels=2, lines=0)
